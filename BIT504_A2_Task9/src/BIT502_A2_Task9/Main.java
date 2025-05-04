c7614fea0d02b93985c3cc23da7d9dbf921c5a16
S-Diggins <shadydiggins1@gmail.com>
2025-05-05 00:24:46 +1200

    added comment to describe program purpose

--- BIT504_A2_Task9/src/BIT502_A2_Task9/Main.java
+++ BIT504_A2_Task9/src/BIT502_A2_Task9/Main.java
@@ -2,6 +2,7 @@ package BIT502_A2_Task9;
 
 public class Main {
 	public static void main(String[] args) {
+		// This is a simple Java program for version control testing
 		System.out.println("This is second commit.");
 	}
 	
