4aa601eb016a25e65209475bc935561973ba6bee
S-Diggins <shadydiggins1@gmail.com>
2025-05-05 00:20:47 +1200

    updated output text in Main method.

--- BIT504_A2_Task9/src/BIT502_A2_Task9/Main.java
+++ BIT504_A2_Task9/src/BIT502_A2_Task9/Main.java
@@ -2,7 +2,7 @@ package BIT502_A2_Task9;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("This is my first commit of Task 9.");
+		System.out.println("This is second commit.");
 	}
 	
 
