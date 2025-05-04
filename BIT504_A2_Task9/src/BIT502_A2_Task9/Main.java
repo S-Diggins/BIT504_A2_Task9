fea11f7593e14cc0ec972b9d6d21c00efa759869
S-Diggins <shadydiggins1@gmail.com>
2025-05-05 00:39:32 +1200

    changed the text output

--- BIT504_A2_Task9/src/BIT502_A2_Task9/Main.java
+++ BIT504_A2_Task9/src/BIT502_A2_Task9/Main.java
@@ -2,7 +2,7 @@ package BIT502_A2_Task9;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("This is second commit.");
+		System.out.println("This is a commit.");
 	}
 	
 
